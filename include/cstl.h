ea9910cdbe61a3abd6b1efadd0d7373753ef9b0f
defg43 <nino.obradovic22@gmail.com>
2025-03-09 00:30:43 +0100

    added stack

--- include/cstl.h
+++ include/cstl.h
@@ -5,7 +5,6 @@
 #define CSTL_realloc realloc
 #define CSTL_dealloc free
 
-// containers
 #define dynarray(T) struct dynarray_##T {					\
 	T *at;													\
 	size_t count;											\
@@ -14,39 +13,192 @@
 
 // this just creates an empty array
 #define create_dynarray(T) ({								\
-	dynarray(T) ret = {										\
+	(dynarray(T)) {											\
 		.at = NULL,											\
 		.count = 0,											\
 		.capacity = 0, 										\
 	};														\
-	return ret;												\
 })
 
+#define dynarray_get_count(array) (array.count)
+
 #define dynarray_append(array, element) ({					\
+	bool return_value = false;								\
 	size_t element_byte_size = sizeof(array.at[0]);			\
+	if(array.capacity == 0) {								\
+		void *alloc_result = 								\
+			CSTL_alloc(4 * element_byte_size);				\
+		if(alloc_result) {									\
+			array.at = alloc_result;						\
+			array.capacity = 4;								\
+			return_value = true;							\
+		} else {											\
+			fprintf(stderr, 								\
+			"failed to allocated memory for dynamic array");\
+			exit(EXIT_FAILURE);								\
+		}													\
+	}														\
+															\
 	if(array.count >= array.capacity) {						\
-		void *realloc_result = NULL;						\
-		if(realloc_result = 								\
+		void *realloc_result =								\
 			CSTL_realloc(array.at, 							\
-			2 * array.capacity * element_byte_size )) {		\
+			2 * array.capacity * element_byte_size);		\
+		if (realloc_result) {								\
 			array.at = realloc_result;						\
 			array.capacity *= 2;							\
 			array.at[array.count] = element;				\
 			array.count++;									\
-			return true;									\
+			return_value = true;							\
+		} else {											\
+			return_value = false;							\
 		}													\
-		return false;										\
 	} else if(array.count < array.capacity) {				\
-		array.at[array.count] =  element;					\
+		array.at[array.count] = element;					\
 		array.count++;										\
+		return_value = true;								\
 	}														\
+	return_value;											\
+})
+
+#define dynarray_insert(array, position, element)	({		\
+	bool return_value = false;								\
+	if (array.count < position) {							\
+		return_value = false;								\
+	} else if (array.count == position) {					\
+		return_value = dynarray_append(array, element);		\
+	} else if (dynarray_append(array, 						\
+		array.at[array.count - 1])) { 						\
+		memmove(array.at + position + 1, 					\
+			array.at + position, 							\
+			(array.count - position) 					\
+			* sizeof(*array.at)); 							\
+		array.at[position] = element;						\
+		return_value = true;								\
+	}														\
+	return_value;											\
+})
+
+#define dynarray_remove(array, position)	({				\
+	bool return_value = false;								\
+	if(position >= array.count) {							\
+		return_value = false;								\
+	} else {												\
+		memmove(array.at + position,						\
+			array.at + position + 1,						\
+			(array.count - position - 1) 					\
+			* sizeof(*array.at));							\
+															\
+		array.count--;										\
+		if(array.count * 2 < array.capacity) {				\
+			void *resize_result = CSTL_realloc(array.at, 	\
+				(sizeof(array.at[0]) * array.capacity) / 2);\
+			if(resize_result == NULL) {						\
+				return_value = false;						\
+			} else {										\
+				return_value = true;						\
+				array.at = resize_result;					\
+				array.capacity /= 2;						\
+			}												\
+		}													\
+	}														\
+	return_value;											\
+})
+
+#define destroy_dynarray(array) ({							\
+	if (array.at) { 										\
+    	CSTL_dealloc(array.at); 							\
+	}														\
+	CSTL_dealloc(array.at);									\
+	array.at = NULL;										\
+	array.count = 0;										\
+	array.capacity = 0;										\
 })
 
 #define stack(T) struct stack_##T {							\
 	T *at;													\
 	size_t count;											\
-	sizt_t capacity;										\
+	size_t capacity;										\
 }
+
+#define create_stack(T) ({               					\
+    (stack(T)) {                         					\
+        .at = NULL,                      					\
+        .count = 0,                       					\
+        .capacity = 0,                     					\
+    };                                   					\
+})
+
+#define stack_get_count(stack) ((stack).count)
+
+#define stack_push(stack, element) ({                             \
+    bool return_value = false;                                    \
+    size_t element_byte_size = sizeof(stack.at[0]);               \
+    if (stack.capacity == 0) {                                    \
+        void *alloc_result = CSTL_alloc(4 * element_byte_size);   \
+        if (alloc_result) {                                       \
+            stack.at = alloc_result;                              \
+            stack.capacity = 4;                                   \
+            return_value = true;                                  \
+        } else {                                                  \
+            fprintf(stderr, "Memory allocation failed\n");        \
+            exit(EXIT_FAILURE);                                   \
+        }                                                         \
+    }                                                             \
+    if (stack.count >= stack.capacity) {                          \
+        void *realloc_result = 									  \
+        	CSTL_realloc(stack.at, 							      \
+        		2 * stack.capacity * element_byte_size); 		  \
+        if (realloc_result) {                                     \
+            stack.at = realloc_result;                            \
+            stack.capacity *= 2;                                  \
+        } else {                                                  \
+            return_value = false;                                 \
+        }                                                         \
+    }                                                             \
+    if (stack.count < stack.capacity) {                           \
+        stack.at[stack.count] = element;                          \
+        stack.count++;                                            \
+        return_value = true;                                      \
+    }                                                             \
+    return_value;                                                 \
+})
+
+#define stack_pop(stack) ({                                       \
+    bool return_value = false;                                    \
+    if (stack.count == 0) {                                       \
+        fprintf(stderr, "Error: Stack underflow\n");              \
+        return_value = false;                                     \
+    } else {                                                      \
+        stack.count--;                                            \
+        if (stack.count * 4 < stack.capacity) {                   \
+            void *resize_result = CSTL_realloc(stack.at,          \
+                (sizeof(stack.at[0]) * stack.capacity) / 2);      \
+            if (resize_result) {                                  \
+                stack.at = resize_result;                         \
+                stack.capacity /= 2;                              \
+            }                                                     \
+        }                                                         \
+        return_value = true;                                      \
+    }                                                             \
+    return_value;                                                 \
+})
+
+#define stack_top(stack) ({                                      \
+    typeof(stack.at[0]) return_value;                            \
+    if (stack.count == 0) {                                      \
+        fprintf(stderr, "Error: Stack is empty\n");              \
+        exit(EXIT_FAILURE);                                      \
+    }                                                            \
+    return_value = stack.at[stack.count - 1];                    \
+    return_value;                                                \
+})
+
+#define destroy_stack(stack) ({                                  \
+    if (stack.at) { CSTL_dealloc(stack.at); }                    \
+    stack.at = NULL;                                             \
+    stack.count = 0;                                             \
+    stack.capacity = 0;                                          \
+})
 
 #define hashtable(T1, T2) struct hashtable_##T1##_##T2 { 	\
 	T1 *key;												\
